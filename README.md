9926ebada0349169b475422b889cf0da246c3f7e
Oscar Montoya <oscar.montoya@puntoscolombia.com>
2022-01-17 10:04:12 -0500

    Save work, all works fine

---
+++ README.md
@@ -0,0 +1,6 @@
+# Link
+https://www.freecodecamp.org/news/aws-cognito-authentication-with-serverless-and-nodejs/
+
+# Repo
+https://github.com/oscardmg/nodejs-cognito-auth
+https://github.com/shivangchauhan7/serverless-auth
--- functions/index.js
+++ functions/index.js
@@ -1,24 +1,23 @@
 const sendResponse = (statusCode, body) => {
-    const response = {
-        statusCode: statusCode,
-        body: JSON.stringify(body),
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Credentials': true
-        }
+  return {
+    statusCode: statusCode,
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
     }
-    return response
-}
+  };
+};
 
 const validateInput = (data) => {
-    const body = JSON.parse(data);
-    const { email, password } = body
-    if (!email || !password || password.length < 6)
-        return false
-    return true
-}
+  const body = JSON.parse(data);
+  const { email, password } = body;
+  if (!email || !password || password.length < 6)
+    return false;
+  return true;
+};
 
 module.exports = {
-    sendResponse, validateInput
+  sendResponse, validateInput
 };
